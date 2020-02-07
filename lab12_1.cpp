0f1342fe17dd29cf2e042c5bba8c8627ee778116
ultramanbabe <ultraman_babeclub@hotmail.com>
2020-02-07 16:49:27 +0700

    Done

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -18,3 +18,28 @@ int main()
 }
 
 //Write definition of stat() here 
+
+void stat(const double A[], int N, double B[]){
+    double max = 0, min = 0;
+    double sumSqr = 0;
+    for(int i = 0; i < N; i++){
+        B[0] += A[i]/N;
+    }
+    
+    for(int i = 0; i < N; i++){
+        sumSqr += pow(A[i],2);
+        B[1] = sqrt((sumSqr/N)-(pow(B[0],2)));
+
+        if(max < A[i]){
+            max = A[i];
+        }
+
+        if(min > A[i]){
+            min = A[i];
+        }
+        B[2] = max;
+        B[3] = min;
+    }
+
+
+}
--- lab12_2.cpp
+++ lab12_2.cpp
@@ -23,3 +23,36 @@ int main()
 }
 
 // Write definition of updateImage() and showImage() here
+
+void updateImage(bool image[N][M], int s, int x, int y){
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            if(sqrt(pow((i - x),2)) + pow((j - y),2) <= s - 1){
+                image[i][j] = 1;
+            }
+        }
+    }
+}
+
+void showImage(const bool image[N][M]){
+    for (int j = 0; j < M + 2; j++){
+        cout << "-";
+    }
+    cout << endl;
+
+    for(int i = 0; i < N; i++){
+        cout << "|";
+        for(int j = 0; j < M; j++){
+            if(image[i][j] == 0){
+                cout << " ";
+            }else{
+                cout << "*";
+            }
+        }
+        cout << "|" << endl;
+    }
+    for (int j = 0; j < M + 2; j++){
+        cout << "-";
+    }
+    cout << endl;
+}
