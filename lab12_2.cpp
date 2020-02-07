377905caa31205aef543ecb3edab291b380d0467
ultramanbabe <ultraman_babeclub@hotmail.com>
2020-02-07 17:03:19 +0700

    Done

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -27,7 +27,7 @@ int main()
 void updateImage(bool image[N][M], int s, int x, int y){
     for(int i = 0; i < N; i++){
         for(int j = 0; j < M; j++){
-            if(sqrt(pow((i - x),2)) + pow((j - y),2) <= s - 1){
+            if((sqrt(pow((i - x),2) + pow((j - y),2))) <= s - 1){
                 image[i][j] = 1;
             }
         }
